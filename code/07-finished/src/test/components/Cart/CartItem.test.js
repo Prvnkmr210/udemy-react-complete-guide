762562b405c43f37604ff87a2f43c269098caca5
Praveen <64293303+Prvnkmr210@users.noreply.github.com>
2023-10-21 18:16:21 +0530

    Update CartItem.test.js

--- code/07-finished/src/test/components/Cart/CartItem.test.js
+++ code/07-finished/src/test/components/Cart/CartItem.test.js
@@ -1,35 +1,67 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import CartItem from '../../components/Cart/CartItem';
+import { screen, fireEvent, render } from '@testing-library/react';
+import CartItem from '../../../components/Cart/CartItem';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('CartItem', () => {
+  let container;
+
   const mockOnAdd = jest.fn();
   const mockOnRemove = jest.fn();
 
   const props = {
-    name: 'Test Item',
+    name: 'TestItem',
     price: 10.00,
     amount: 2,
     onAdd: mockOnAdd,
     onRemove: mockOnRemove,
   };
 
-  it('renders correctly', () => {
-    render(<CartItem {...props} />);
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
 
-    expect(screen.getByText('Test Item')).toBeInTheDocument();
-    expect(screen.getByText('$10.00')).toBeInTheDocument();
-    expect(screen.getByText('x 2')).toBeInTheDocument();
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+
+  
+
+  it('renders correctly, rener in react 17', () => {
+    // render(<CartItem {...props} />);
+    // expect(screen.getByText('$10.00')).toBeInTheDocument();
+
+    // different way of render using container
+    const { getByText } = render(<CartItem {...props} />, container);
+    console.log(getByText);
+    expect(screen.getByText('TestItem')).toBeInTheDocument();    
+    
+    // this works with; import ReactDOM from 'react-dom';
+    // ReactDOM.render(<CartItem {...props} />, container);
+    // this queryselector works only with ReactDom.render not with the testing library render
+    // expect(container.querySelector('.cart-item .price').textContent).toBe(`$${props.price.toFixed(2)}`);
+    // expect(container.querySelector('.cart-item .amount').textContent).toBe(`x ${props.amount}`);
+  });
+
+  it('React 18 way of rendering the component', () => {
+    // https://stackoverflow.com/questions/71685441/react-testing-library-gives-console-error-for-reactdom-render-in-react-18
+
+    // const root = ReactDOM.createRoot(container);
+    // ReactDOM.createRoot(container).render(<CartItem {...props} />);
+    render(<CartItem {...props} />);
+    expect(screen.getByText('TestItem')).toBeInTheDocument();
   });
 
   it('calls onAdd when + button is clicked', () => {
-    render(<CartItem {...props} />);
+    render(<CartItem {...props} />, container);
 
     fireEvent.click(screen.getByText('+'));
     expect(mockOnAdd).toHaveBeenCalled();
   });
 
   it('calls onRemove when - button is clicked', () => {
-    render(<CartItem {...props} />);
+    render(<CartItem {...props} />, container);
 
     fireEvent.click(screen.getByText('−'));
     expect(mockOnRemove).toHaveBeenCalled();
